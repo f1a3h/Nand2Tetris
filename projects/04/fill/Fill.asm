fc4ef8caa406656c81fc81ab9990c1d8674f749d
f1a3h <ihlwer@outlook.com>
2023-08-06 01:10:16 +0800

    complete Fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,64 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// Pseudo code:
+// LOOP:
+//     i = 8192;
+//     if (KBD == 0) WHITEN;
+//     else BLACKEN;
+//     LOOP;
+// WHITEN:
+//     i = i - 1;
+//     addr = SCREEN + i;
+//     *addr = 0;
+//     if (i == 0) LOOP;
+//     else WHITEN;
+// BLACKEN:
+//     i = i - 1;
+//     addr = SCREEN + i;
+//     *addr = -1;
+//     if (i == 0) LOOP;
+//     else BLACKEN;
+
+(LOOP)
+// i = 8192
+@8192
+D=A
+@i
+M=D
+
+@KBD
+D=M
+@WHITEN
+D;JEQ
+
+(BLACKEN)
+// i = i - 1
+@i
+M=M-1
+D=M
+// addr = SCREEN + i
+@SCREEN
+A=A+D
+M=-1
+
+@LOOP
+D;JEQ
+@BLACKEN
+0;JMP
+
+(WHITEN)
+// i = i - 1
+@i
+M=M-1
+D=M
+// addr = SCREEN + i
+@SCREEN
+A=A+D
+M=0
+
+@LOOP
+D;JEQ
+@WHITEN
+0;JMP
