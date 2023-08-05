1a986c2430e0fae0e17500478517e15f9273a33d
f1a3h <ihlwer@outlook.com>
2023-08-06 01:24:39 +0800

    complete Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,47 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// Pseudo code:
+// i = *R0
+// *R2 = 0
+// LOOP:
+//     if (i == 0) END;
+//     else MULT;
+// MULT:
+//     i = i - 1;
+//     (*R2) = (*R1) + (*R2);
+//     JMP LOOP;
+
+// i = *R0
+@R0
+D=M
+@i
+M=D
+
+// *R2 = 0
+@R2
+M=0
+
+(LOOP)
+@i
+D=M
+@END
+D; JEQ
+
+(MULT)
+// i = i - 1
+@i
+M=M-1
+// (*R2) = (*R1) + (*R2)
+@R1
+D=M
+@R2
+M=D+M
+// JMP LOOP
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
